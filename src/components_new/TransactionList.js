7f1ce8b3cc787c4104339d3dae7d4785c94daee1
Saim Saqib <saimmoin64@gmail.com>
2020-12-24 18:26:55 +0500

    Changed Directory Name

--- src/components_new/TransactionList.js
+++ src/components_new/TransactionList.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import {GlobalContext} from '../context/GlobalState'
+import {GlobalContext} from '../context_new/GlobalState'
 import {Transaction} from './Transaction'
 
 export const TransactionList = () => {
