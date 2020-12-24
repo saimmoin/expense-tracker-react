daca0c991d710b181d3f2e74de1e7737df8fe390
Saim Saqib <saimmoin64@gmail.com>
2020-12-24 18:29:33 +0500

    Changed Directory Name

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import {GlobalContext} from '../context_new/GlobalState'
+import {GlobalContext} from '../context/GlobalState'
 
 export const Transaction = ({transaction}) => {
   const {deleteTransaction} = useContext(GlobalContext);
